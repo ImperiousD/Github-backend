633b97aba357506788d44e143d7a81e3fd997256
Eduardo <67244018+Edfreak@users.noreply.github.com>
2025-11-11 20:02:18 -0400

    Update main.py

---
+++ python/basic/main.py
@@ -0,0 +1,25 @@
+""" Comentario
+de varias 
+lineas 
+pal 
+profe """
+
+## One line
+
+#List - Listas Nos sirven para guardar una coleccion de datos
+lista = [1, 2, 3, 4, 5]
+names = ["delvis", "Ivan", "Sofia","Andreina"]
+print("Variables de listas:")
+print(type(lista))
+print(type(names))
+print(names)
+
+#tuple - Tuplas Nos sirven para guardar una coleccion de datos
+tupla = (1, 2, 3, 4, {"delvis", "Ivan", "Sofia","Andreina"}, [1, 2, 3, 4, 5]) # Las tuplas son inmutables
+print("Variables de tuplas:")
+print(type(tupla))
+
+#dict - Diccionarios Nos sirven para guardar una coleccion de datos
+diccionario = {"name": "delvis", "age": 20, "city": "Css"}
+print("Variables de diccionarios:")
+print(type(diccionario))
