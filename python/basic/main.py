caef3b8bfe4d0e217c52a545311deae37c3566dc
Eduardo <67244018+Edfreak@users.noreply.github.com>
2025-11-14 00:40:22 -0400

    Clase actualizadas

--- python/basic/main.py
+++ python/basic/main.py
@@ -7,7 +7,7 @@ profe """
 ## One line
 
 #List - Listas Nos sirven para guardar una coleccion de datos
-lista = [1, 2, 3, 4, 5]
+"""lista = [1, 2, 3, 4, 5]
 names = ["delvis", "Ivan", "Sofia","Andreina"]
 print("Variables de listas:")
 print(type(lista))
@@ -22,4 +22,39 @@ print(type(tupla))
 #dict - Diccionarios Nos sirven para guardar una coleccion de datos
 diccionario = {"name": "delvis", "age": 20, "city": "Css"}
 print("Variables de diccionarios:")
-print(type(diccionario))
+print(type(diccionario))"""
+
+"Ejercicio 1:"
+
+edad = 20
+tiene_boleto = True
+
+puede_entrar = (edad >= 18) and (tiene_boleto == True)
+print(puede_entrar)
+
+"Ejercicio 2:"
+
+promedio = 8.5
+distancia_km = 60
+
+califica_beca = (promedio >= 9) or (distancia_km >= 50)
+print(califica_beca)
+
+"Ejercicio 3:"
+
+rol = "editor"
+esta_activo = True
+
+tiene_acceso_admin = (rol == "admin") or (rol == "editor") and (esta_activo == True)  
+print(tiene_acceso_admin)
+
+"Ejercicio 4:"
+
+temp = 35
+
+es_temperatura_peligrosa = not (temp >= 20 and temp <= 30)
+print(es_temperatura_peligrosa)
+
+
+
+## convertir varias lineas en comentario shift + alt + a
---
+++ python/basic/operadores_de_asignacion.py
@@ -0,0 +1,14 @@
+## asignacion simple
+
+num1 = 24
+
+## asignacion compuesta de suma (+=) esto sumara la cantidad que pongamos en la misma variable
+
+num1 += 5
+
+## asignacion compuesta de resta (-=) lo mismo
+
+num1 -= 15
+
+## division compuesta: /=, modulo compuesto %=, multiplicacion compuesta *=
+
